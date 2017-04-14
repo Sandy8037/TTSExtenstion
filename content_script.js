809cfd281d955451b81f7c5cb776b7c14316830d
Sandeep Devarapalli <Sandeep Devarapalli>
2017-04-14 12:50:14 -0700

    Text to speech(TTS)

--- content_script.js
+++ content_script.js
@@ -4,7 +4,28 @@ att.value = 'mouseUp()';
 body[0].setAttributeNode(att);
 
 var script = document.createElement('script');
-var src = "function mouseUp() { var selObj, selectText, text; selObj = window.getSelection(); selectText = selObj.toString(); text = selectText || ''; talk(text); } function talk(text) { var msg = new SpeechSynthesisUtterance(text); speechSynthesis.speak(msg); }";
+var src = "function mouseUp() { var selObj, selectText, text; selObj = window.getSelection(); selectText = selObj.toString(); text = selectText || ''; talk(text); } function talk(text) { var msg = new SpeechSynthesisUtterance(text); speechSynthesis.speak(msg); } ";
 var tt = document.createTextNode(src);
 script.appendChild(tt);
 document.getElementsByTagName('head')[0].appendChild(script);
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.state === 'pause') {
+      speechSynthesis.pause();
+    }
+});
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.state === 'resume') {
+      speechSynthesis.resume();
+    }
+});
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.state === 'cancel') {
+      speechSynthesis.cancel();
+    }
+});
--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
 chrome.tabs.executeScript(null, {file: 'content_script.js'});
+
+document.getElementById('pause').addEventListener('click', function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {state: "pause"}, function () {
+    });
+  });
+});
+
+document.getElementById('resume').addEventListener('click', function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {state: "resume"}, function () {
+    });
+  });
+});
+
+document.getElementById('cancel').addEventListener('click', function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {state: "cancel"}, function () {
+    });
+  });
+});
