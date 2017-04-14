f415d28a1e6669198dac1b932b87b7e3073c9e43
Sandeep Devarapalli <Sandeep Devarapalli>
2017-04-14 14:43:24 -0700

    Text to speech(TTS)

--- content_script.js
+++ content_script.js
@@ -4,7 +4,7 @@ att.value = 'mouseUp()';
 body[0].setAttributeNode(att);
 
 var script = document.createElement('script');
-var src = "function mouseUp() { var selObj, selectText, text; selObj = window.getSelection(); selectText = selObj.toString(); text = selectText || ''; talk(text); } function talk(text) { var msg = new SpeechSynthesisUtterance(text); speechSynthesis.speak(msg); } ";
+var src = "var msg, selObj, selectText, text; function mouseUp() { selObj = window.getSelection(); selectText = selObj.toString(); text = selectText || ''; talk(text); } function talk(text) { msg = new SpeechSynthesisUtterance(text); speechSynthesis.speak(msg); } ";
 var tt = document.createTextNode(src);
 script.appendChild(tt);
 document.getElementsByTagName('head')[0].appendChild(script);
